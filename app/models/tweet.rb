d75ca7c163c0ba5d08851df6c72cfe6d69f5b265
faxathome <parafajardo@gmail.com>
2018-01-18 11:42:04 +0100

    Added Usernames & Names

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
-  validates :user, presence: :true
-  validates :content, presence: :true, length: {maximum: 140}
+  validates :user, presence: true
+  validates :content, presence: true, length: {maximum: 140}
 end
