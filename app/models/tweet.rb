7429136769f53e6c9681c62b1e2af2ef4f5a0753
faxathome <parafajardo@gmail.com>
2018-01-31 19:16:59 +0100

    Fix AWS bucket endpoint

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,6 +4,7 @@ class Tweet < ApplicationRecord
   validates :user, presence: true
   validates :content, presence: true, length: {maximum: 140}
 
-  has_attached_file :image
+  has_attached_file :image, :styles =>
+    { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.invertersale.com/media/product_medium/165_image-not-found.gif"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
